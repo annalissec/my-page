aedd0907cfa4a14acd65dce76a998a9ed92af935
annalisse chang <annalissechang@southern.edu>
2022-05-17 17:11:51 -0400

    .

--- my-app/src/components/topbar/Topbar.jsx
+++ my-app/src/components/topbar/Topbar.jsx
@@ -1,11 +1,21 @@
 import "./topbar.scss"
+import {} from "feather-icons"
 
 export default function 
 () {
   return (
 	<div className='topbar'>
 		<div className="wrapper">
-			hello
+			<div className="left">
+				<a href="#contact" className="logo">annalisse chang</a>
+				<div className="itemContainer">
+					<i data-feather='user'></i>
+				</div>
+			</div>
+
+			<div className="right">
+				<a href="#portfolio">portfolio</a>
+			</div>
 		</div>
 	</div>
   )
